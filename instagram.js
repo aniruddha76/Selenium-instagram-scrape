4ec01db6115cbf6f50a917f6edb30d05e7a5abb4
aniruddha <aniruddhathakur76@gmail.com>
2024-01-19 22:14:34 +0530

    Fixed scroll issue, but still needs to improve

--- instagram.js
+++ instagram.js
@@ -1,9 +1,26 @@
-import { Builder, By, Key, until } from 'selenium-webdriver';
+import { Builder, By, Condition, Key, until } from 'selenium-webdriver';
 import { parse } from 'node-html-parser';
 
-async function fetchInstagramPage() {
+let cdnLinks = new Set();
 
-  let cdnLinks  = new Set();
+async function getImages(driver) {
+  let images = await driver.findElement(By.tagName('article')).getAttribute('outerHTML');
+      let document = parse(images)
+
+      document.querySelectorAll('img').map(imgElement => {
+        let links = imgElement.getAttribute('src')
+        cdnLinks.add(links)
+      })
+}
+
+async function scrollDown(driver) {
+  await driver.sleep(5000)
+
+  await driver.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+  await driver.sleep(5000);
+}
+
+async function fetchInstagramPage() {
 
   const driver = await new Builder().forBrowser('chrome').build();
 
@@ -11,36 +28,28 @@ async function fetchInstagramPage() {
     await driver.get('https://www.instagram.com/saraya/');
 
     await driver.wait(until.elementLocated(By.className('_aagu')), 30000);
-    
-      let images = await driver.findElement(By.tagName('article')).getAttribute('outerHTML');
-      let document = parse(images)
 
-      document.querySelectorAll('img').map(imgElement => {
-        let links = imgElement.getAttribute('src')
-        cdnLinks.add(links)
-      })
+      getImages(driver);
 
       let endLoginBanner = await driver.findElement(By.className('_abn5 '));
       endLoginBanner.click();
 
       let showMorePosts = await driver.findElement(By.className('_any9'));
       showMorePosts.click();
-      
-      let images2 = await driver.findElement(By.tagName('article')).getAttribute('outerHTML');
-      let document2 = parse(images2)
 
-      document2.querySelectorAll('img').map(imgElement => {
-        let links = imgElement.getAttribute('src')
-        cdnLinks.add(links)
-      })
-      
+      scrollDown(driver);
+      await getImages(driver);
 
-    return cdnLinks;
+      await scrollDown(driver);
+      await getImages(driver);
+
   } finally {
     await driver.quit();
+    return cdnLinks
   }
 }
 
 fetchInstagramPage().then(html => {
   console.log(html)
 });
+
